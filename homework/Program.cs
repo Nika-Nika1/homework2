dfa2dc68ae589709b84a182deade46533c64a6fd
Nikoloz Lursmanashvili <nikolozlursmanashvili01@gmail.com>
2026-05-30 12:35:39 +0400

    1-davaleba

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, user!");
+Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your age");
+int age = int.Parse(Console.ReadLine());
+if (age < 18){
+    Console.WriteLine($"Sorry {name} you cant vote for now");
+}
+else{
+    Console.WriteLine($"{name} You can vote ");
+}
